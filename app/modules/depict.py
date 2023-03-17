c6440bda7deef608d16e9c27b719900ebc69de6b
Kohulan <kohulan.rajan@uni-jena.de>
2023-03-17 23:51:48 +0100

    fix: scaling and atom colouring

--- app/modules/depict.py
+++ app/modules/depict.py
@@ -1,12 +1,12 @@
+import xml.etree.ElementTree as ET
 from rdkit import Chem
 from rdkit.Chem import rdDepictor
 from rdkit.Chem.Draw import rdMolDraw2D
 from app.modules.cdkmodules import getCDKSDG
-import xml.etree.ElementTree as ET
 from jpype import JClass
 
 
-def getCDKDepiction(smiles: str, molSize=(512, 512), rotate=0):
+def getCDKDepiction(smiles: str, molSize=(512, 512), rotate=0, unicolor=False):
     """This function takes the user input SMILES and Depicts it
        using the CDK Depiction Generator.
     Args:
@@ -19,21 +19,31 @@ def getCDKDepiction(smiles: str, molSize=(512, 512), rotate=0):
     StandardGenerator = JClass(
         cdk_base + ".renderer.generators.standard.StandardGenerator"
     )
-    DepictionGenerator = JClass(cdk_base + ".depict.DepictionGenerator")()
     Color = JClass("java.awt.Color")
     UniColor = JClass(cdk_base + ".renderer.color.UniColor")
+    CDK2DAtomColors = JClass(cdk_base + ".renderer.color.CDK2DAtomColors")()
+    if unicolor:
+        DepictionGenerator = (
+            JClass(cdk_base + ".depict.DepictionGenerator")()
+            .withSize(molSize[0], molSize[1])
+            .withAtomValues()
+            .withParam(StandardGenerator.StrokeRatio.class_, 1.0)
+            .withAnnotationColor(Color.BLACK)
+            .withParam(StandardGenerator.AtomColor.class_, UniColor(Color.BLACK))
+            .withBackgroundColor(Color.WHITE)
+            .withFillToFit()
+        )
+    else:
+        DepictionGenerator = (
+            JClass(cdk_base + ".depict.DepictionGenerator")()
+            .withAtomColors(CDK2DAtomColors)
+            .withSize(molSize[0], molSize[1])
+            .withAtomValues()
+            .withParam(StandardGenerator.StrokeRatio.class_, 1.0)
+            .withFillToFit()
+            .withBackgroundColor(Color.WHITE)
+        )
 
-    # Generate depiction with settings
-    DepictionGenerator.withSize(molSize[0], molSize[1]).withAtomValues().withParam(
-        StandardGenerator.StrokeRatio.class_, 1.0
-    ).withAnnotationColor(Color.BLACK).withParam(
-        StandardGenerator.AtomColor.class_, UniColor(Color.BLACK)
-    ).withBackgroundColor(
-        Color.WHITE
-    ).withZoom(
-        2.0
-    )
-    getString = JClass("java.lang.String")
     moleculeSDG = getCDKSDG(smiles)
 
     # Rotate molecule
@@ -42,20 +52,10 @@ def getCDKDepiction(smiles: str, molSize=(512, 512), rotate=0):
         moleculeSDG, point, (rotate * JClass("java.lang.Math").PI / 180.0)
     )
 
-    mol_image = DepictionGenerator.depict(moleculeSDG)
-    mol_imagex = mol_image.toSvgStr(getString("px")).getBytes()
+    mol_imageSVG = DepictionGenerator.depict(moleculeSDG).toSvgStr("px").getBytes()
+    encoded_image = ET.tostring(ET.fromstring(mol_imageSVG), encoding="unicode")
 
-    # Fix scaling
-    mol_root = ET.fromstring(mol_imagex)
-    new_width = molSize[0]
-    new_height = molSize[1]
-    mol_root.set("width", "{}px".format(new_width))
-    mol_root.set("height", "{}px".format(new_height))
-
-    # Write the modified SVG element to a string
-    rescaled_mol = ET.tostring(mol_root, encoding="unicode")
-
-    return rescaled_mol
+    return encoded_image
 
 
 def getRDKitDepiction(smiles, molSize=(512, 512), rotate=0, kekulize=True):
