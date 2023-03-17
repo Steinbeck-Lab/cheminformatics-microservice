0b1f620296cff5b1b76fff291492650fd518f912
Venkata Chandra Sekhar Nainala <mailcs76@gmail.com>
2023-03-17 17:17:43 +0100

    feat: customised fastapi swagger interface

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 # from .config import settings
 from .routers import converters, chem, compose, decimer
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.openapi.utils import get_openapi
 
 app = FastAPI()
 
@@ -20,3 +21,22 @@ app.include_router(converters.router)
 app.include_router(chem.router)
 app.include_router(compose.router)
 app.include_router(decimer.router)
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Cheminf Micro Services",
+        version="0.3.0",
+        description="This set of essential and valuable microservices is designed to be accessed via API calls to support cheminformatics. Generally, it is designed to work with SMILES-based inputs and could be used to translate between different machine-readable representations, get Natural Product (NP) likeliness scores, visualize chemical structures, and generate descriptors. In addition, the microservices also host an instance of STOUT and another instance of DECIMER (two deep learning models for IUPAC name generation and optical chemical structure recognition, respectively).",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://github.com/Steinbeck-Lab/cheminformatics-python-microservice/raw/main/public/img/logo.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
