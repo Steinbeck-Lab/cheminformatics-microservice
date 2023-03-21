8537b83d30084dff5c577c7ec86dbf92f3d86885
Venkata Chandra Sekhar Nainala <mailcs76@gmail.com>
2023-03-21 13:05:22 +0100

    fix: linting issue resolution

--- app/main.py
+++ app/main.py
@@ -35,7 +35,7 @@ def custom_openapi():
         return app.openapi_schema
     openapi_schema = get_openapi(
         title="Cheminf Micro Services",
-        version = os.getenv("RELEASE_VERSION", "pre-release"),
+        version=os.getenv("RELEASE_VERSION", "pre-release"),
         description="This set of essential and valuable microservices is designed to be accessed via API calls to support cheminformatics. Generally, it is designed to work with SMILES-based inputs and could be used to translate between different machine-readable representations, get Natural Product (NP) likeliness scores, visualize chemical structures, and generate descriptors. In addition, the microservices also host an instance of STOUT and another instance of DECIMER (two deep learning models for IUPAC name generation and optical chemical structure recognition, respectively).",
         routes=app.routes,
     )
