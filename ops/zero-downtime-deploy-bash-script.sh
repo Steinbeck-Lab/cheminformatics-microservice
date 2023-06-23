283be2dc98628dc235455a6221a23366f0e2974a
NishaSharma14 <sharmanis14@gmail.com>
2023-06-23 14:27:07 +0200

    fix: error fix in zero-time deployment script.

--- ops/zero-downtime-deploy-bash-script.sh
+++ ops/zero-downtime-deploy-bash-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define variables
-COMPOSE_FILE="/mnt/data/cheminformatics-python-microservice/docker-compose-dev.yml"
+COMPOSE_FILE="/mnt/data/cheminformatics-python-microservice/ops/docker-compose-dev.yml"
 DOCKER_REPO_NAME="caffeinejena/cheminformatics-python-microservice:dev-latest"
 IMAGE_NAME="caffeinejena/cheminformatics-python-microservice:dev-latest"
 NEW_CONTAINER_ID=""
@@ -24,9 +24,6 @@ check_health() {
 
 # Check if there is a new image available in the Docker repository
 if [ "$(docker pull $DOCKER_REPO_NAME | grep "Status: Image is up to date" | wc -l)" -eq 0 ]; then
-  # Pull the latest image
-   echo "Pull the latest image.."
-   docker-compose -f $COMPOSE_FILE pull
 
   # Scale up a new container
    echo "Scale up new container.."
@@ -45,7 +42,7 @@ if [ "$(docker pull $DOCKER_REPO_NAME | grep "Status: Image is up to date" | wc 
          if ! check_health; then
                 n=$(( $n + 1 ))
                 sleep 1m
-                echo "Container not healthy.. Check once more.."
+                echo "Container not healthy.. Check again.."
          else
                 IS_CONTAINER_HEALTHY=0
                 break
@@ -53,42 +50,38 @@ if [ "$(docker pull $DOCKER_REPO_NAME | grep "Status: Image is up to date" | wc 
    done
 
   # Remove old containers and images
-   if [ $IS_CONTAINER_HEALTHY == 0 ] ; then
+  if [ $IS_CONTAINER_HEALTHY == 0 ] ; then
+  
+        # Set the desired container name prefix
+        CONTAINER_NAME_PREFIX="ops_web"
 
-        # Get the container IDs of running containers with the same image
-        container_ids=$(docker ps --filter ancestor=$IMAGE_NAME --format "{{.ID}}")
+        # Retrieve the container IDs that match the prefix
+        container_ids=$(docker ps -a --filter "name=^/${CONTAINER_NAME_PREFIX}" --format "{{.ID}}")
 
-        # Check if there are exactly two containers running with the same image
-        if [[ $(echo "$container_ids" | wc -l) -eq 2 ]]; then
+        # Sort the container IDs by creation date in ascending order
+        sorted_container_ids=$(echo "$container_ids" | xargs docker inspect --format='{{.Created}} {{.ID}}' | sort | awk '{print $2}')
 
-                # Get the creation timestamps of the two containers
-                timestamp1=$(docker inspect --format="{{.Created}}" $container_ids | head -n 1)
-                timestamp2=$(docker inspect --format="{{.Created}}" $container_ids | tail -n 1)
+        # Get the oldest container ID
+        oldest_container_id=$(echo "$sorted_container_ids" | head -n 1)
 
-                # Compare the timestamps to determine the older container
-                if [[ "$timestamp1" < "$timestamp2" ]]; then
-                        container_to_delete=$container_ids
-                else
-                        container_to_delete=$(echo "$container_ids" | tail -n 1)
-                fi
+        # Check if any container IDs were found
+        if [[ -z "$oldest_container_id" ]]; then
+                echo "No containers found with the name prefix '${CONTAINER_NAME_PREFIX}'."
+                exit 1
+        fi
 
-                # Delete the older container and images
-                docker stop $container_to_delete
-                docker rm $container_to_delete
-                docker image prune -af
+        # Delete the old container and unused images
+        docker stop $oldest_container_id
+        docker rm $oldest_container_id
+        docker image prune -af
+        echo "Deleted the oldest container with ID: ${oldest_container_id}"
 
-                echo "Deleted container $container_to_delete"
-
-                else
-                        echo "Two running containers with the same image not found"
-                fi
-
-   else
+  else
         echo "Couldnot complete the deployment as the container is unhealthy.."
         docker stop $NEW_CONTAINER_ID
         docker rm $NEW_CONTAINER_ID
-   fi
+  fi
 
 else
-        echo "Skipping deployment as no new image available.."
+       echo "Skipping deployment as no new image available.."
 fi
