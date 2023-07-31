025d9615433f0abb3ad4f72355273c6d6a030b1a
Venkata Chandra Sekhar Nainala <mailcs76@gmail.com>
2023-07-31 16:55:05 +0200

    fix: fastapi_versioning - CORS middleware conflict issue fix (added custom middleware)

--- app/main.py
+++ app/main.py
@@ -24,16 +24,6 @@ app = FastAPI(
     },
 )
 
-origins = ["*"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 app.include_router(chem.router)
 app.include_router(converters.router)
 app.include_router(depict.router)
@@ -59,6 +49,27 @@ app = VersionedFastAPI(
 )
 
 Instrumentator().instrument(app).expose(app)
+
+origins = ["*"]
+
+
+@app.middleware("http")
+async def add_cors_headers(request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    return response
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/", include_in_schema=False)
