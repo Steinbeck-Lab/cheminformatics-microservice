bc2a8d3bec67d6181bc1d9a703d571cecbcca4cf
Chandu Nainala <mailcs76@gmail.com>
2023-08-31 13:57:27 +0200

    fix: Custom domain update - removing base path from the vitepress config (#350)

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Cheminformatics Python Microservice",
   description: "This set of essential and valuable microservices is designed to be accessed via API calls to support cheminformatics. Generally, it is designed to work with SMILES-based inputs and could be used to translate between different machine-readable representations, get Natural Product (NP) likeliness scores, visualize chemical structures, and generate descriptors. In addition, the microservices also host an instance of STOUT and another instance of DECIMER (two deep learning models for IUPAC name generation and optical chemical structure recognition, respectively).",
-  base: '/cheminformatics-python-microservice/',
 
   themeConfig: {
     logo: { 
