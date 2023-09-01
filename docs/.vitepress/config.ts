9ddca88b5b434130cc6188a01874a1c481403b25
NishaSharma14 <sharmanis14@gmail.com>
2023-09-01 10:37:31 +0200

    docs: enable search

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,7 +5,11 @@ export default defineConfig({
   title: "Cheminformatics Python Microservice",
   description: "This set of essential and valuable microservices is designed to be accessed via API calls to support cheminformatics. Generally, it is designed to work with SMILES-based inputs and could be used to translate between different machine-readable representations, get Natural Product (NP) likeliness scores, visualize chemical structures, and generate descriptors. In addition, the microservices also host an instance of STOUT and another instance of DECIMER (two deep learning models for IUPAC name generation and optical chemical structure recognition, respectively).",
 
-  themeConfig: {    
+  themeConfig: {
+    search: {
+      provider: 'local'
+    },
+    
     logo: { 
       light: 'logo.png',
       dark: 'logo_light.png'
