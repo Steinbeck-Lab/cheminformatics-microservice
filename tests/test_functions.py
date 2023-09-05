97419afa20ec556ac0245f7cc2c4175bc53da427
Venkata Chandra Sekhar Nainala <mailcs76@gmail.com>
2023-09-05 19:10:11 +0200

    wip: handling invalid input in a consistent and logical way - fixing tests

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -2,72 +2,82 @@ import pytest
 import selfies as sf
 from app.modules.depiction import getRDKitDepiction, getCDKDepiction
 from app.modules.npscorer import getNPScore
-
+from app.modules.toolkits.helpers import parseInput
 
 @pytest.fixture
 def test_smiles():
     return "CN1C=NC2=C1C(=O)N(C(=O)N2C)C"
 
+@pytest.fixture
+def test_RDKit_Mol():
+    smiles = "CN1C=NC2=C1C(=O)N(C(=O)N2C)C"
+    return parseInput(smiles, "rdkit", False)
 
-def test_npscore(test_smiles):
+@pytest.fixture
+def test_CDK_Mol():
+    smiles = "CN1C=NC2=C1C(=O)N(C(=O)N2C)C"
+    return parseInput(smiles, "cdk", False)
+
+
+def test_npscore(test_RDKit_Mol):
     expected_result = "-1.09"
-    actual_result = getNPScore(test_smiles)
+    actual_result = getNPScore(test_RDKit_Mol)
     assert expected_result == actual_result
 
 
 # RDKit Depiction tests
-def test_getRDKitDepiction(test_smiles):
-    svg = getRDKitDepiction(test_smiles)
+def test_getRDKitDepiction(test_RDKit_Mol):
+    svg = getRDKitDepiction(test_RDKit_Mol)
     assert isinstance(svg, str)
     assert "svg" in svg
     assert "Error" not in svg
 
 
-def test_getRDKitDepiction_kekulize(test_smiles):
-    svg = getRDKitDepiction(test_smiles, kekulize=False)
+def test_getRDKitDepiction_kekulize(test_RDKit_Mol):
+    svg = getRDKitDepiction(test_RDKit_Mol, kekulize=False)
     assert isinstance(svg, str)
     assert "svg" in svg
     assert "Error" not in svg
 
 
-def test_getRDKitDepiction_rotate(test_smiles):
-    svg = getRDKitDepiction(test_smiles, rotate=90)
+def test_getRDKitDepiction_rotate(test_RDKit_Mol):
+    svg = getRDKitDepiction(test_RDKit_Mol, rotate=90)
     assert isinstance(svg, str)
     assert "svg" in svg
     assert "Error" not in svg
 
 
-def test_getRDKitDepiction_size(test_smiles):
-    svg = getRDKitDepiction(test_smiles, molSize=(512, 512))
+def test_getRDKitDepiction_size(test_RDKit_Mol):
+    svg = getRDKitDepiction(test_RDKit_Mol, molSize=(512, 512))
     assert isinstance(svg, str)
     assert "svg" in svg
     assert "Error" not in svg
 
 
 # CDK depiction tests
-def test_getCDKDepiction(test_smiles):
-    svg = getCDKDepiction(test_smiles)
+def test_getCDKDepiction(test_CDK_Mol):
+    svg = getCDKDepiction(test_CDK_Mol)
     assert isinstance(svg, str)
     assert "svg" in svg
     assert "Error" not in svg
 
 
-def test_getCDKDepiction_unicolor(test_smiles):
-    svg = getCDKDepiction(test_smiles, unicolor=True)
+def test_getCDKDepiction_unicolor(test_CDK_Mol):
+    svg = getCDKDepiction(test_CDK_Mol, unicolor=True)
     assert isinstance(svg, str)
     assert "svg" in svg
     assert "Error" not in svg
 
 
-def test_getCDKDepiction_rotate(test_smiles):
-    svg = getCDKDepiction(test_smiles, rotate=90)
+def test_getCDKDepiction_rotate(test_CDK_Mol):
+    svg = getCDKDepiction(test_CDK_Mol, rotate=90)
     assert isinstance(svg, str)
     assert "svg" in svg
     assert "Error" not in svg
 
 
-def test_getCDKDepiction_size(test_smiles):
-    svg = getCDKDepiction(test_smiles, molSize=(512, 512))
+def test_getCDKDepiction_size(test_CDK_Mol):
+    svg = getCDKDepiction(test_CDK_Mol, molSize=(512, 512))
     assert isinstance(svg, str)
     assert "svg" in svg
     assert "Error" not in svg
