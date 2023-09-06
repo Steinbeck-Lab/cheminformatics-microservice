38a523783942fcee77490e6da278204cb8f3e41f
NishaSharma14 <sharmanis14@gmail.com>
2023-09-06 14:08:58 +0200

    test: write test for ocsr module

--- tests/test_ocsr.py
+++ tests/test_ocsr.py
@@ -5,19 +5,23 @@ from app.main import app
 
 client = TestClient(app)
 
+
 def test_ocsr_index():
     response = client.get("/latest/ocsr/")
     assert response.status_code == 200
     assert response.json() == {"status": "OK"}
 
+
 @pytest.mark.parametrize(
     "input, response_code",
     [
-        ('{"path": "https://static-content.springer.com/image/art%3A10.1186%2Fs13321-023-00744-6/MediaObjects/13321_2023_744_Figa_HTML.png","reference": "xyzzy"}', 200),
+        (
+            '{"path": "https://static-content.springer.com/image/art%3A10.1186%2Fs13321-023-00744-6/MediaObjects/13321_2023_744_Figa_HTML.png","reference": "xyzzy"}',
+            200,
+        ),
         ("INVALID_INPUT", 422),
     ],
 )
-
 def test_process(input, response_code):
     response = client.post(
         "/latest/ocsr/process", data=input, headers={"Content-Type": "application/json"}
