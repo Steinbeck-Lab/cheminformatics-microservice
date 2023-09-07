4b85afdfddf9941a5fa11de42173c30ff8614a68
Nisha Sharma <54287612+NishaSharma14@users.noreply.github.com>
2023-09-07 11:26:44 +0200

    fix: writing missing test (#374)
    * test: test commit to check code coverage
    
    * test: update test command
    
    * test: test code coverage
    
    * test: test code coverage
    
    * test: test code coverage
    
    * test: test code coverage
    
    * test: test coverage report
    
    * test: code coverage report
    
    * test: test code coverage
    
    * test: test code coverage
    
    * ci: add action for code coverage
    
    * test: add missing test to chem.py
    
    * test: remove braces enclosing response code
    
    * test: fix errors in test_chem.py
    
    * linting
    
    * test: update response text
    
    * chore: delete compose.py
    
    * test: update exception response code
    
    * test: update test for converters
    
    * test: fix typo
    
    * test: remove extra parameter
    
    * test: fix errors in converter test
    
    * test: update test for depict and linting
    
    * test: write test for ocsr
    
    * test: fix issues for test
    
    * test: write test for ocsr module
    
    * test: write test for tools
    
    * linting
    
    * test_depict2D_molecule
    
    * fix: added missing surge dependencies and corrected url query parameters in tests
    
    * test: use sudo to move surge
    
    * test: reformat chem test
    
    * test: fix typo in tool test
    
    * test: fix parameter issue
    
    * test: fix input error in chem test
    
    * test: update repo name in code coverage
    
    * test: update the trigger point
    
    ---------
    
    Co-authored-by: Venkata Chandra Sekhar Nainala <mailcs76@gmail.com>

--- tests/test_depict.py
+++ tests/test_depict.py
@@ -12,30 +12,34 @@ def test_chem_index():
     assert response.json() == {"status": "OK"}
 
 
-@pytest.fixture
-def test_smiles():
-    return "CN1C=NC2=C1C(=O)N(C(=O)N2C)C"
-
-
 @pytest.mark.parametrize(
-    "smiles, generator, width, height, rotate, CIP, unicolor",
+    "smiles, generator, width, height, rotate, CIP, unicolor,response_code",
     [
-        ("CCO", "cdk", 512, 512, 0, False, False),
-        ("CCO", "rdkit", 512, 512, 0, False, False),
+        ("CCO", "cdk", 512, 512, 0, False, False, 200),
+        ("CCO", "rdkit", 512, 512, 0, False, False, 200),
+        ("INVALID_INPUT", "cdk", 512, 512, 0, False, False, 422),
     ],
 )
-def test_depict2D_molecule(smiles, generator, width, height, rotate, CIP, unicolor):
+def test_depict2D_molecule(
+    smiles, generator, width, height, rotate, CIP, unicolor, response_code
+):
     response = client.get(
         f"/latest/depict/2D?smiles={smiles}&generator={generator}&width={width}&height={height}&rotate={rotate}&CIP={CIP}&unicolor={unicolor}"
     )
-    assert response.status_code == 200
-    assert response.headers["content-type"] == "image/svg+xml"
+    assert response.status_code == response_code
 
 
-def test_depict3D_molecule(test_smiles):
-    response = client.get(f"/latest/depict/3D?smiles={test_smiles}")
-    assert response.status_code == 200
-    assert response.headers["content-type"] == "text/html; charset=utf-8"
+@pytest.mark.parametrize(
+    "smiles, toolkit,response_code",
+    [
+        ("CCO", "openbabel", 200),
+        ("CCO", "rdkit", 200),
+        ("INVALID_INPUT", "openbabel", 422),
+    ],
+)
+def test_depict3D_molecule(smiles, toolkit, response_code):
+    response = client.get(f"/latest/depict/3D?smiles={smiles}&toolkit={toolkit}")
+    assert response.status_code == response_code
 
 
 # Run the tests
