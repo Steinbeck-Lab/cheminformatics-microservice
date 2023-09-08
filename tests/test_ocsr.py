9f4101133b2b0f31fa49b430b396277d3f30464a
NishaSharma14 <sharmanis14@gmail.com>
2023-09-08 10:16:52 +0200

    test: write test for process_upload

--- tests/test_ocsr.py
+++ tests/test_ocsr.py
@@ -29,6 +29,22 @@ def test_process(input, response_code):
     assert response.status_code == response_code
 
 
+def test_process_upload():
+    with open("caffeine.png", "rb") as file:
+        files = {"file": ("caffeine.png", file, "image/png")}
+
+        response = client.post(
+            "/latest/ocsr/process-upload",
+            file=files,
+            headers={"Content-Type": "multipart/form-data"},
+        )
+        assert response.status_code == 200
+        assert (
+            response.text
+            == '"{"reference": null,"smiles": ["CN1C=NC2=C1C(=O)N(C)C(=O)N2C"]}"'
+        )
+
+
 # Run the tests
 if __name__ == "__main__":
     pytest.main()
