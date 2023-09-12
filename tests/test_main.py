ae660227b430e68a11b0c84364cdf4b2697815ea
Kohulan <kohulan.rajan@uni-jena.de>
2023-09-12 22:55:48 +0200

    fix: main test to cover root

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi.testclient import TestClient
 from app.main import app
 
@@ -8,6 +9,13 @@ def test_root_redirect():
     response = client.get("/latest/docs")
     assert response.status_code == 200
     assert response.headers["content-type"] == "text/html; charset=utf-8"
+
+
+def test_root_endpoint():
+    response = client.get("/")
+    assert response.status_code == 200
+    expected_url = "http://testserver" + os.getenv("HOMEPAGE_URL", "/latest/docs")
+    assert response.url == expected_url
 
 
 def test_get_health():
