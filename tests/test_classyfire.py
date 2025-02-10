1868b3883cc5c3f79390c8835804f5083add18d4
Kohulan <kohulan.rajan@uni-jena.de>
2025-02-10 13:05:55 +0100

    fix: classyfire tests

--- tests/test_classyfire.py
+++ tests/test_classyfire.py
@@ -2,41 +2,41 @@ import pytest
 from app.modules.classyfire import classify, result
 import asyncio
 
-
 @pytest.fixture
 def valid_smiles():
     return "CN1C=NC2=C1C(=O)N(C(=O)N2C)C"
-
 
 @pytest.fixture
 def invalid_smiles():
     return "invalid_smiles"
 
-
-def test_valid_classyfire(valid_smiles):
-    loop = asyncio.get_event_loop()
-    result_ = loop.run_until_complete(classify(valid_smiles))
+@pytest.mark.asyncio
+async def test_valid_classyfire(valid_smiles):
+    result_ = await classify(valid_smiles)
     assert result_["query_type"] == "STRUCTURE"
     id_ = result_["id"]
+    
     while True:
-        classified = loop.run_until_complete(result(id_))
+        classified = await result(id_)
         if classified["classification_status"] == "Done":
             break
-        asyncio.run(asyncio.sleep(2))
+        await asyncio.sleep(2)
+    
     assert classified["classification_status"] == "Done"
     assert classified["entities"][0]["class"]["name"] == "Imidazopyrimidines"
 
-
-def test_invalid_classyfire(invalid_smiles):
-    loop = asyncio.get_event_loop()
-    result_ = loop.run_until_complete(classify(invalid_smiles))
+@pytest.mark.asyncio
+async def test_invalid_classyfire(invalid_smiles):
+    result_ = await classify(invalid_smiles)
     assert result_["query_input"] == "invalid_smiles"
     id_ = result_["id"]
+    
     while True:
-        classified = loop.run_until_complete(result(id_))
+        classified = await result(id_)
         if classified["classification_status"] == "Done":
             break
-        asyncio.run(asyncio.sleep(2))
+        await asyncio.sleep(2)
+    
     assert classified["classification_status"] == "Done"
     assert (
         classified["invalid_entities"][0]["report"][0]
